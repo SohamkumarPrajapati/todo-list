b7d0e844bb5b99a62ab45ce2ea49be802b489bab
Sohamkumar Prajapati <Sohamkumar.Prajapati@zeuslearning.mumbai>
2025-07-18 14:51:31 +0530

    header component finished with its styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
-    <header id="main-header">
-        <div>{{ title }}</div>
+    <header id="mainheader">
+        <div id="titleHeader">
+            <div id="logo"></div>
+            <div id="title">{{ title }}</div>
+        </div>
         <div id="userIcon"></div>
     </header>
 </template>
@@ -10,7 +13,7 @@
 <script>
 export default {
     name: 'Header',
-    data: function() {
+    data: function () {
         return {
             title: 'To Do',
         }
@@ -19,4 +22,44 @@ export default {
 </script>
 
 <style>
+#mainheader {
+    height: 40px;
+    background-color: rgb(45, 196, 151);
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+#titleHeader {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#logo {
+    height: 30px;
+    width: 40px;
+    background-image: url('../assets/images/logo.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    border-radius: 5px;
+}
+
+#title {
+    color: antiquewhite;
+    font-size: larger;
+    font-family: "Roboto", sans-serif;
+    font-weight: bold;
+    padding-left: 10px;
+}
+
+#userIcon {
+    height: 20px;
+    width: 50px;
+    background-image: url('../assets/images/user-solid.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+}
 </style>
